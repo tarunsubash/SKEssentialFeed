85c0c946e5aab894cad199d40ff3426fb48a70d2
subash <tarun.subash01@gmail.com>
2023-10-23 10:24:18 +0530

    setup Feeditem models

---
+++ FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  SKEssentialFeed
+//
+//  Created by Subash on 23/10/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let uuid: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
