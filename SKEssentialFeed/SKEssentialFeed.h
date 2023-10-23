954b5bbc51c8fb1bfcadd4a871563acac6b492db
subash <tarun.subash01@gmail.com>
2023-10-23 10:17:01 +0530

    feed framework setup

---
+++ SKEssentialFeed/SKEssentialFeed.h
@@ -0,0 +1,18 @@
+//
+//  SKEssentialFeed.h
+//  SKEssentialFeed
+//
+//  Created by Subash on 23/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SKEssentialFeed.
+FOUNDATION_EXPORT double SKEssentialFeedVersionNumber;
+
+//! Project version string for SKEssentialFeed.
+FOUNDATION_EXPORT const unsigned char SKEssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKEssentialFeed/PublicHeader.h>
+
+
