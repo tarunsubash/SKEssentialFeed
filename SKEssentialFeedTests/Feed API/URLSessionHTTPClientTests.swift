bd30c7aa8d2fa98c501ce7a87bdf84d58374414f
subash <tarun.subash01@gmail.com>
2024-02-04 17:28:20 +0530

    Make `URLSessionHTTPClient` confirm to `HTTPClient` and move the file to production module

--- SKEssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ SKEssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -9,27 +9,6 @@
 import XCTest
 import SKEssentialFeed
 
-class URLSessionHTTPClient {
-    private let session: URLSession
-    
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-    struct UnexpectedValuesRepresentation: Error {}
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data,
-                      let response = response as? HTTPURLResponse {
-                completion(.success(response, data))
-            } else {
-                completion(.failure(UnexpectedValuesRepresentation()))
-            }
-        }.resume()
-    }
-}
-
 class URLSessionHTTPClientTests: XCTestCase {
     override func setUp() {
         super.setUp()
@@ -92,7 +71,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath,
-                         line: UInt = #line) -> URLSessionHTTPClient {
+                         line: UInt = #line) -> HTTPClient {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
