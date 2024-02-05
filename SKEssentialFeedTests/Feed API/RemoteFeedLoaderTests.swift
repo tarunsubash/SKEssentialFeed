f71dfdf472fd89c832f690cfe303b492bfd2ee47
subash <tarun.subash01@gmail.com>
2024-02-05 10:41:35 +0530

    Update Response Field `uuid` to `id` to match the actual response

--- SKEssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ SKEssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -140,7 +140,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let item = FeedItem(uuid: uuid, description: description, location: location, imageURL: imageURL)
         
         let json = [
-            "uuid": uuid.uuidString,
+            "id": uuid.uuidString,
             "description": description,
             "location": location,
             "image": imageURL.absoluteString
