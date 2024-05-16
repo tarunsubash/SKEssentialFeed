97b60cf3c279f448e1d18795bf4f2656a77d454a
subash <tarun.subash01@gmail.com>
2024-05-16 21:12:06 +0530

    Save command request new cache insertion on successful deletion of old cache

--- SKEssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ SKEssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -16,19 +16,36 @@ class LocalFeedLoader {
     }
     
     func save(_ items: [FeedItem]) {
-        store.deleteCachedFeed()
+        store.deleteCachedFeed { [unowned self] error in
+            if error == nil {
+                store.insert(items)
+            }
+        }
     }
 }
 
 class FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
     var deletedCacheFeedCount = 0
     var insertCallCount = 0
-    func deleteCachedFeed() {
+    
+    private var deletionCompletions = [DeletionCompletion]()
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         deletedCacheFeedCount += 1
+        deletionCompletions.append(completion)
     }
     
     func completeDeletion(with error: Error, at index: Int = 0) {
-        
+        deletionCompletions[index](error)
+    }
+    
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        deletionCompletions[index](nil)
+    }
+    
+    func insert(_ items: [FeedItem]) {
+        insertCallCount += 1
     }
 }
 
@@ -57,6 +74,16 @@ final class CacheFeedUseCaseTests: XCTestCase {
         store.completeDeletion(with: deletionError)
         
         XCTAssertEqual(store.insertCallCount, 0)
+    }
+    
+    func test_save_requestNewCacheInsertionOnsuccessfulDeletion() {
+        let (sut, store) = makeSUT()
+        let items = [uniqueItem()]
+        
+        sut.save(items)
+        store.completeDeletionSuccessfully()
+        
+        XCTAssertEqual(store.insertCallCount, 1)
     }
     
     // MARK: - Helpers:
