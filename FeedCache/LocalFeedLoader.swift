cff97575dcebf10863a19dde21e02acac89b410c
subash <tarun.subash01@gmail.com>
2024-05-23 07:04:26 +0530

    Load command delivers no images on empty cache

--- FeedCache/LocalFeedLoader.swift
+++ FeedCache/LocalFeedLoader.swift
@@ -41,6 +41,8 @@ public final class LocalFeedLoader {
         store.retrieve { error in
             if let error = error {
                 completion(.failure(error))
+            } else {
+                completion(.success([]))
             }
         }
     }
