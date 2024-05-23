4f535479b5d27d3fbcdb5eccd0f5128f5886423d
subash <tarun.subash01@gmail.com>
2024-05-23 06:59:22 +0530

    Add Result type to load command

--- FeedCache/LocalFeedLoader.swift
+++ FeedCache/LocalFeedLoader.swift
@@ -12,6 +12,8 @@ public final class LocalFeedLoader {
     let currentDate: () -> Date
     
     public typealias SaveResult =  Error?
+    public typealias LoadResult =  LoadFeedResult
+    
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
@@ -35,8 +37,12 @@ public final class LocalFeedLoader {
         }
     }
     
-    public func load(completion: @escaping (Error?) -> Void) {
-        store.retrieve(completion: completion)
+    public func load(completion: @escaping (LoadResult) -> Void) {
+        store.retrieve { error in
+            if let error = error {
+                completion(.failure(error))
+            }
+        }
     }
 }
 
