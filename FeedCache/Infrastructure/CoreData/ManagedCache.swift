9056db9adb68894fa7ed478a6fb757a1aaa43938
subash <tarun.subash01@gmail.com>
2024-06-05 20:40:54 +0530

    Separate CoreData managed model classes data from helpers with extensions

--- FeedCache/Infrastructure/CoreData/ManagedCache.swift
+++ FeedCache/Infrastructure/CoreData/ManagedCache.swift
@@ -11,7 +11,9 @@ import CoreData
 class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
-    
+}
+
+extension ManagedCache {
     static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
         try find(in: context).map(context.delete)
         return ManagedCache(context: context)
