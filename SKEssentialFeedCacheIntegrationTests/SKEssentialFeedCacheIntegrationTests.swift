d525aa0cc8843e52bdb63d602e2b8b27c2962d53
subash <tarun.subash01@gmail.com>
2024-06-06 07:42:40 +0530

    Clean up & undo all cache side-effects on setUp and tearDown to avoid sharing state between tests

--- SKEssentialFeedCacheIntegrationTests/SKEssentialFeedCacheIntegrationTests.swift
+++ SKEssentialFeedCacheIntegrationTests/SKEssentialFeedCacheIntegrationTests.swift
@@ -9,6 +9,19 @@ import XCTest
 import SKEssentialFeed
 
 final class SKEssentialFeedCacheIntegrationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        undoStoreSideEffects()
+    }
+    
     func test_load_deliversNoItemsOnEmptyCache() throws {
         let sut = try makeSUT()
         
@@ -44,5 +57,17 @@ final class SKEssentialFeedCacheIntegrationTests: XCTestCase {
     
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
 }
