8148f63a5eab1c44da06d7ecbafbf6269c4060ce
subash <tarun.subash01@gmail.com>
2025-05-31 19:04:24 +0530

    Move ViewModels to separate files

---
+++ SKEssentialFeediOS/FeedUI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedLoadingViewModel.swift
+//  SKEssentialFeediOS
+//
+//  Created by Subash on 31/05/25.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
+
---
+++ SKEssentialFeediOS/FeedUI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  SKEssentialFeediOS
+//
+//  Created by Subash on 31/05/25.
+//
+
+import SKEssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
