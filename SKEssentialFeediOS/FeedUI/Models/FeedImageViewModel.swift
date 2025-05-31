f52f9b06640f8f1a76c974810356219365b2298a
subash <tarun.subash01@gmail.com>
2025-05-31 07:22:26 +0530

    Move FeedImage value translations (including image loading state) to `FeedImageViewModel`. Now, the `FeedImageCellController` acts as a binder between the `View` and the `ViewModel`

---
+++ SKEssentialFeediOS/FeedUI/Models/FeedImageViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  FeedImageViewModel.swift
+//  SKEssentialFeediOS
+//
+//  Created by Subash on 31/05/25.
+//
+
+import SKEssentialFeed
+import UIKit
+import Foundation
+
+public final class FeedImageViewModel {
+    typealias Observer<T> = (T) -> Void
+    
+    private var task: FeedImageDataLoaderTask?
+    private var model: FeedImage
+    private var imageLoader: FeedImageDataLoader
+    
+    init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+        self.model = model
+        self.imageLoader = imageLoader
+    }
+    
+    var description: String? { model.description }
+    
+    var location: String? { model.location }
+    
+    var hasLocation: Bool { location != nil }
+    
+    var onImageLoad: Observer<UIImage>?
+    var onImageLoadingStateChange: Observer<Bool>?
+    var onShouldRetryImageLoadStateChange: Observer<Bool>?
+    
+    func loadImageData() {
+        onImageLoadingStateChange?(true)
+        onShouldRetryImageLoadStateChange?(false)
+        task = imageLoader.loadImageData(from: model.url) { [weak self] result in
+            self?.handle(result)
+        }
+    }
+    
+    private func handle(_ result: FeedImageDataLoader.Result) {
+        if let image = (try? result.get()).flatMap(UIImage.init) {
+            onImageLoad?(image)
+        } else {
+            onShouldRetryImageLoadStateChange?(true)
+        }
+        onImageLoadingStateChange?(false)
+    }
+    
+    func cancelImageDataLoad() {
+        task?.cancel()
+        task = nil
+    }
+    
+}
