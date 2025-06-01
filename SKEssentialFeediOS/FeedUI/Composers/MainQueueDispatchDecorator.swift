1b8e073053372db4c4f2d8c278d546389e65a5ee
subash <tarun.subash01@gmail.com>
2025-06-01 11:54:51 +0530

    Move MainQueueDispatchDecorator to separate file

---
+++ SKEssentialFeediOS/FeedUI/Composers/MainQueueDispatchDecorator.swift
@@ -0,0 +1,45 @@
+//
+//  MainQueueDispatchDecorator.swift
+//  SKEssentialFeediOS
+//
+//  Created by Subash on 01/06/25.
+//
+
+import Foundation
+import SKEssentialFeed
+
+final class MainQueueDispatchDecorator<T> {
+    private let decoratee: T
+    
+    init(decoratee: T) {
+        self.decoratee = decoratee
+    }
+    
+    func dispatch(_ completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async(execute: completion)
+        }
+        completion()
+    }
+}
+
+extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            self?.dispatch({
+                completion(result)
+            })
+        }
+    }
+}
+
+extension MainQueueDispatchDecorator: FeedImageDataLoader where T ==  FeedImageDataLoader {
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> any FeedImageDataLoaderTask {
+        decoratee.loadImageData(from: url) { [weak self] result in
+            self?.dispatch {
+                completion(result)
+            }
+        }
+    }
+}
