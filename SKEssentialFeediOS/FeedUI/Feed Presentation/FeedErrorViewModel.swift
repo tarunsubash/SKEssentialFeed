adcafd0b86536d85c88356e43bbcb541498c21d8
subash <tarun.subash01@gmail.com>
2025-06-03 08:25:56 +0530

    Add FeedErrorViewModel factory helpers to clarify intent in the point of use

--- SKEssentialFeediOS/FeedUI/Feed Presentation/FeedErrorViewModel.swift
+++ SKEssentialFeediOS/FeedUI/Feed Presentation/FeedErrorViewModel.swift
@@ -8,4 +8,12 @@
 
 struct FeedErrorViewModel {
     let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: .none)
+    }
+    
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
 }
