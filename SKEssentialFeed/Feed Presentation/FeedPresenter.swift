0e0eab09c6397cb0febb5e0b6b986c3afd876502
subash <tarun.subash01@gmail.com>
2025-06-05 12:46:51 +0530

    Move FeedViewModel to separate file

--- SKEssentialFeed/Feed Presentation/FeedPresenter.swift
+++ SKEssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -5,10 +5,6 @@
 //  Created by Subash on 05/06/25.
 //
 
-public struct FeedViewModel {
-    public let feed: [FeedImage]
-}
-
 public protocol FeedView {
     func display(_ viewModel: FeedViewModel)
 }
---
+++ SKEssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedViewModel.swift
+//  SKEssentialFeed
+//
+//  Created by Subash on 05/06/25.
+//
+
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
